050b3bf7354495aa9d097670a36acce2ec60f2b1
Sergio Barrera <sdbeng@gmail.com>
2015-07-29 11:41:12 -0700

    Initial Commit

---
+++ CoreDataBeta4UITests/CoreDataBeta4UITests.swift
@@ -0,0 +1,34 @@
+//
+//  CoreDataBeta4UITests.swift
+//  CoreDataBeta4UITests
+//
+//  Created by admin 33 on 7/29/15.
+//  Copyright © 2015 sdbwebsolutions. All rights reserved.
+//
+
+import XCTest
+
+class CoreDataBeta4UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
